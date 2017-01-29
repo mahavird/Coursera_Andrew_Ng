ed612c768b3078cc6e87694d2b4c2e90cc924e9f
mahavird <mahaviredx@gmail.com>
2017-01-30 01:03:41 +0530

    Solution with Explaination

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -5,6 +5,9 @@ function p = predict(Theta1, Theta2, X)
 
 % Useful values
 m = size(X, 1);
+n = size(X, 2);
+fprintf('\No of Input (training Examples) to NN %i\n',m);
+fprintf('\Size of features of every Training Example to NN %i\n',n);
 num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
@@ -20,13 +23,30 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
-X = [ones(m, 1) X];
+
+fprintf('\Inside Neural network predict function\n');
+
+X = [ones(m, 1) X];     %adds 1 to all the rows of X (bias unit)
 
 a2 = sigmoid(X * Theta1');
-a2 = [ones(m, 1) a2];
+
+m1 = size(a2, 1);
+n1 = size(a2, 2);
+fprintf('\No of rows in a2 %i\n',m1);
+fprintf('\ No of cols in a2 %i\n',n1)
+
+a2 = [ones(m, 1) a2];   %adds 1 to all the rows of a2
 htheta = sigmoid(a2 * Theta2');
 
-[temp, p] = max(htheta, [], 2);
+
+m2 = size(htheta, 1);
+n2 = size(htheta, 2);
+fprintf('\No of rows in htheta %i\n',m2);
+fprintf('\ No of cols in htheta %i\n',n2)
+
+printf("%i\n", htheta);
+
+[temp, p] = max(htheta, [], 2); % Returns Max value among cols of each row of htheta matrix
 
 
 
